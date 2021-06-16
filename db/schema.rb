dc7bdac54d8a97772fe8d4b894f70ef52dafa6d7
Umang Gupta <umanggupta.iet@gmail.com>
2021-06-16 14:49:54 +0530

    Added admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_132726) do
+ActiveRecord::Schema.define(version: 2021_06_16_091217) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_132726) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "is_admin", default: false
   end
 
 end
